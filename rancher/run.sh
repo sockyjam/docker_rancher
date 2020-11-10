56a889d7031fcf2df6d1d4b4c9e476e21403cf5b
sockyjam@126.com <sockyjam@126.com>
2020-11-10 14:28:29 +0800

    a

--- rancher/run.sh
+++ rancher/run.sh
@@ -1,2 +1,2 @@
-docker run -d --restart=unless-stopped -v $PWD/data:/var/lib/rancher/ -p 8060:80 -p 442:443 --name rancher rancher/rancher:stable
+docker run -d --restart=unless-stopped -v $PWD/data:/var/lib/rancher/ -p 8060:80 -p 442:443 --name rancher rancher/rancher:latest
 
