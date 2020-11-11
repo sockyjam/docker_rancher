606ff3980f844e00a9ed0cc8ee405d48d7456c58
sockyjam@126.com <sockyjam@126.com>
2020-11-11 20:31:33 +0800

    runs 2.4.6

---
+++ rancher/run3.sh
@@ -0,0 +1,7 @@
+docker run -d --restart=unless-stopped -p 8060:80 -p 442:443 \
+	-v $PWD/docker_volume/rancher_home/cni:/var/lib/cni \
+	-v $PWD/docker_volume/rancher_home/kubelet:/var/lib/kubelet \
+	-v $PWD/docker_volume/rancher_home/rancher:/var/lib/rancher \
+	-v $PWD/docker_volume/rancher_home/log:/var/log/log \
+	--name rancher rancher/rancher:v2.4.6
+
